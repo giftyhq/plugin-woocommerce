7eaff13ebc42ca32df9e9ab86b35a71e9bc8c116
Jeroen van de Weerd <jeroen@gifty.nl>
2022-01-19 18:01:00 +0100

    Update tested version
    Update the tested WooCommerce version

--- gifty-woocommerce.php
+++ gifty-woocommerce.php
@@ -11,7 +11,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Requires PHP: 7.2
  * Requires at least: 5.4
- * Tested up to: 5.6
+ * Tested up to: 5.8
  * WC requires at least: 4.4.0
  * WC tested up to: 6.1.0
  */
