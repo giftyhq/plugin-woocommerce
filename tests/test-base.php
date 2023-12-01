63d550f3e416e618bfecb8ed8eee597fb7cf330e
Jeroen van de Weerd <jeroen@gifty.nl>
2023-12-01 17:13:10 +0100

    Add automated test suite (#18)
    * Added automated test suite
    * Updated to new Gifty-PHP library for support of PHP 7.3

---
+++ tests/test-base.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Gifty_Woocommerce
+ */
+
+/**
+ * Sample test case.
+ */
+class BaseTest extends WP_UnitTestCase {
+
+	/**
+	 * Test the plugin can be initialized
+	 */
+	public function test_plugin_boot() {
+        $this->assertTrue( true );
+	}
+}
